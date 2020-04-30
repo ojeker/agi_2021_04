9be904dc2519cdadd7e8e12be4762d22fe82764f
uncoyote <jeker.git@outlook.de>
2020-04-30 14:57:10 +0200

    var

---
+++ consume/consume_api.mm
@@ -0,0 +1,30 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1574926204925" ID="ID_830516258" MODIFIED="1574926245966" TEXT="Struktur">
+<node CREATED="1574926248000" ID="ID_1039137656" MODIFIED="1574926254623" POSITION="right" TEXT="Aufgabe">
+<node CREATED="1574926254630" ID="ID_1167384649" MODIFIED="1574926305882" TEXT="Sonnenpfad: Aufruf des Drilldown-Service mit Antwort innerhalb von X Sekunden"/>
+<node CREATED="1574926313165" ID="ID_1355627214" MODIFIED="1574926324491" TEXT="Input">
+<node CREATED="1574926326156" ID="ID_805434681" MODIFIED="1574926329033" TEXT="Koordinaten"/>
+</node>
+<node CREATED="1574926330188" ID="ID_118849687" MODIFIED="1574926334732" TEXT="Output">
+<node CREATED="1574926334739" ID="ID_570666730" MODIFIED="1574926364885" TEXT="Ergebnisse des Drilldown als Attribute">
+<node CREATED="1574926366093" ID="ID_72121882" MODIFIED="1574927299459" TEXT="Bemerkungsfeld umfasst json string[], aus dem die Zeilenumbr&#xfc;che folgen"/>
+</node>
+<node CREATED="1574926414207" ID="ID_767951822" MODIFIED="1574926426714" TEXT="Geometrie des geklickten Punktes"/>
+<node CREATED="1574926554430" ID="ID_708207127" MODIFIED="1574926590240" TEXT="Fehlerinformation">
+<node CREATED="1574926590246" ID="ID_1368947234" MODIFIED="1574926604547" TEXT="Benutzerverst&#xe4;ndliche R&#xfc;ckmeldung"/>
+<node CREATED="1574926604733" ID="ID_133645665" MODIFIED="1574926621114" TEXT="Details in das Logfile"/>
+</node>
+</node>
+<node CREATED="1574928943855" ID="ID_354527118" MODIFIED="1574928951820" TEXT="Zu nutzende Bibliotheken:">
+<node CREATED="1574928952975" ID="ID_680865864" MODIFIED="1574928982332" TEXT="HTTP-Client: Requests oder urllib, abh&#xe4;ngig von der einzusetzenden Python-Version"/>
+<node CREATED="1574929007281" ID="ID_701752154" MODIFIED="1574929789972" TEXT="Konfiguration: json verwenden, da sowieso bekannt sein muss f&#xfc;r json lesen / schreiben"/>
+<node CREATED="1574929021087" ID="ID_323105043" MODIFIED="1574929231293" TEXT="Logging: logging"/>
+</node>
+<node CREATED="1574930078840" ID="ID_928050322" MODIFIED="1574930083225" TEXT="Fragen:">
+<node CREATED="1574930083230" ID="ID_1594817372" MODIFIED="1574930094530" TEXT="Sollen wir dies &#xfc;berhaupt selber machen --&gt; Stefan"/>
+<node CREATED="1574930095143" ID="ID_577946008" MODIFIED="1574931398539" TEXT="Python-Version --&gt; Matthias"/>
+</node>
+</node>
+</node>
+</map>
